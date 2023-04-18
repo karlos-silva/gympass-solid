0845cc1fd17b2082d93ced233c464e1b7b684df1
Karlos Silva <karlossilva194@gmail.com>
2023-04-18 14:57:00 -0300

    feat: create checkin use-case and create-gym use-case

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,21 +1,106 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './check-in'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { MaxDistanceError } from './errors/max-distance-error'
+import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-in-error'
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let sut: CheckInUseCase
 
 describe('Check-in Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
-    sut = new CheckInUseCase(checkInsRepository)
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+    gymsRepository.create({
+      id: '1',
+      title: 'Gym Test',
+      description: null,
+      phone: null,
+      latitude: 0,
+      longitude: 0,
+    })
+
+    vi.useFakeTimers()
   })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
   it('should be able to check in', async () => {
     const { checkIn } = await sut.execute({
       gymId: '1',
       userId: '1',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check twice in the same day', async () => {
+    vi.setSystemTime(new Date(2023, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: '1',
+      userId: '1',
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: '1',
+        userId: '1',
+        userLatitude: 0,
+        userLongitude: 0,
+      }),
+    ).rejects.toBeInstanceOf(MaxNumberOfCheckInsError)
+  })
+
+  it('should be able to check twice but in different days', async () => {
+    vi.setSystemTime(new Date(2023, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: '1',
+      userId: '1',
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    vi.setSystemTime(new Date(2023, 0, 21, 8, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      gymId: '1',
+      userId: '1',
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.create({
+      id: '2',
+      title: 'Gym Test',
+      description: null,
+      phone: null,
+      latitude: -25.4541518,
+      longitude: -49.2537338,
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: '2',
+        userId: '1',
+        userLatitude: -25.4322176,
+        userLongitude: -49.2091018,
+      }),
+    ).rejects.toBeInstanceOf(MaxDistanceError)
   })
 })
